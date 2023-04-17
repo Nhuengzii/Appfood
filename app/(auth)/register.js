5d140a5df14242554f81e83ebf3a64d69c109d1f
Nhuengzii <32nhueng@gmail.com>
2023-04-17 21:10:57 +0700

    create template for register and login

--- app/(auth)/register.js
+++ app/(auth)/register.js
@@ -1,22 +1,22 @@
-import { Text, View, TextInput } from "react-native";
+import { Text, View, TextInput, Button } from "react-native";
 import React, { useState } from "react";
 import { useAuth } from "../../context/auth";
 import { useRouter } from "expo-router";
 
-export default function SignIn() {
-  const { signIn, signInWithEmailAndPassword } = useAuth();
+export default function Register() {
+  const { signIn, createUserWithEmailAndPassword } = useAuth();
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
-  const router = useRouter();
+  const router = useRouter()
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 44, fontWeight: "bold" }}>This is Sign-In page</Text>
-      <Text>Username</Text>
+      <Button title="<" onPress={() => router.back()} />
+      <Text style={{ fontSize: 44, fontWeight: "bold" }}>This is register page</Text>
+      <Text>Email</Text>
       <TextInput value={userName} onChangeText={setUserName} style={{ borderColor: "black", borderWidth: 1, padding: 10, height: 40, margin: 12 }} />
       <Text>Password</Text>
       <TextInput value={password} onChangeText={setPassword} style={{ borderColor: "black", borderWidth: 1, padding: 10, height: 40, margin: 12 }} />
-      <Text onPress={() => router.push("/register")} style={{ backgroundColor: "red", fontSize: 40 }}>Register</Text>
-      <Text onPress={() => signInWithEmailAndPassword(userName, password)} style={{ backgroundColor: "green", fontSize: 40 }}>Sign In</Text>
+      <Text onPress={() => createUserWithEmailAndPassword(userName, password)} style={{ backgroundColor: "red", fontSize: 40 }}> Create Account</Text>
     </View>
   );
 }
--- app/index.js
+++ app/index.js
@@ -3,10 +3,15 @@ import { Text, View, Button } from "react-native";
 import { useAuth } from "../context/auth";
 
 export default function Index() {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+  if (user === null) {
+    return
+  }
+  console.log(JSON.stringify(user, null, 4))
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text> This is home screen</Text>
+      <Text> You are {user.email}</Text>
       <Button title={"Sign Out"} onPress={() => signOut()} />
     </View>
   );
