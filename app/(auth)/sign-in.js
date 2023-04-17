24764a3d7dc9a509418d864c47ddf0bf1243b0fd
Peeba <baramee.254450@gmail.com>
2023-04-18 02:24:34 +0700

    edit sign-in.js vertion2

--- app/(auth)/sign-in.js
+++ app/(auth)/sign-in.js
@@ -1,8 +1,146 @@
-import { Text, View, TextInput, StyleSheet } from "react-native";
+import { Text, View, TextInput, StyleSheet,ScrollView,SafeAreaView} from "react-native";
 import React, { useState } from "react";
 import { useAuth } from "../../context/auth";
 import { useRouter } from "expo-router";
 
+export default function SignIn() {
+  const { signIn, signInWithEmailAndPassword } = useAuth();
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          // Try setting `flexDirection` to `"row"`.
+          flexDirection: "column",
+        },
+      ]}
+    >
+
+      <View style={styles.flex1}>
+        <Text style={[styles.Texttitle, styles.marginatitle]}>
+          ลงชื่อเข้าใช้
+        </Text>
+      </View>
+      <View style={styles.flex3}>
+        <Text style={[styles.Textfont, styles.text_name_pass]}>ชื่อผู้ใช้</Text>
+        <TextInput
+          value={userName}
+          onChangeText={setUserName}
+          style={styles.TextInput_name_pass}
+        />
+        <Text style={[styles.Textfont, styles.text_name_pass]}>รหัสผ่าน</Text>
+        <TextInput
+          value={password}
+          onChangeText={setPassword}
+          style={styles.TextInput_name_pass}
+          secureTextEntry={true}
+        />
+        <View style={styles.router}>
+          <Text
+            onPress={() => signInWithEmailAndPassword(userName, password)}
+            style={[styles.button, styles.signIn]}
+          >
+            ลงชื่อเข้าใช้
+          </Text>
+          <Text
+            onPress={() => router.push("/register")}
+            style={[styles.forget, styles.Textfont]}
+          >
+            ลืมรหัสผ่าน?
+          </Text>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={[styles.Textfont]}>หรือ </Text>
+            <Text
+              onPress={() => router.push("/register")}
+              style={[styles.Textfont, styles.router]}
+            >
+              สร้างบัญชี
+            </Text>
+          </View>
+        </View>
+        
+      </View>
+      
+      <View style={styles.flex1}></View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 10,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  flex1: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "flex-end",
+  },
+  flex2: {
+    flex: 2,
+    backgroundColor: "red",
+  },
+  flex3: {
+    flex: 3,
+    backgroundColor: "#FEF6AE",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  Textfont: {
+    fontSize: 20,
+  },
+  Texttitle: {
+    fontSize: 30,
+  },
+  marginatitle: {
+    marginLeft: 10,
+    marginVertical: 20,
+  },
+  TextInput_name_pass: {
+    borderColor: "black",
+    borderBottomWidth: 1,
+    height: 40,
+    fontSize: 24,
+    marginTop: 10,
+    marginVertical: 30,
+  },
+  text_name_pass: {
+    marginTop: 20,
+    marginVertical: 20,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  signIn: {
+    textAlign: "center",
+    color: "white",
+    width: 229,
+    backgroundColor: "#FF783F",
+    fontSize: 24,
+    marginVertical: 12,
+  },
+  forget: {
+    backgroundColor: "#FEF6AE",
+    marginVertical: 12,
+  },
+  router: {
+    backgroundColor: "#FEF6AE",
+    alignItems: "center",
+    color: "#D12121",
+    
+  },
+});
+
+/*
 export default function SignIn() {
   const { signIn, signInWithEmailAndPassword } = useAuth();
   const [userName, setUserName] = useState("");
@@ -59,6 +197,7 @@ export default function SignIn() {
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#FEF6AE",
@@ -74,15 +213,15 @@ const styles = StyleSheet.create({
     fontSize: 24,
   },
   text_name_pass: {
-    marginTop: 30,
-    marginVertical: 30,
+    marginTop: 20,
+    marginVertical: 20,
   },
   TextInput_name_pass: {
     borderColor: "black",
     borderBottomWidth: 1,
     height: 40,
-    fontSize: 20,
-    marginTop: 30,
+    fontSize: 24,
+    marginTop: 10,
     marginVertical: 30,
   },
   textfont: {
@@ -94,15 +233,17 @@ const styles = StyleSheet.create({
     width: 229,
     backgroundColor: "#FF783F",
     fontSize: 32,
-    margin: 12,
+    marginVertical: 12,
   },
   forget: {
-    backgroundColor: "#FF783F",
-    margin: 12,
+    backgroundColor: "#FEF6AE",
+    marginVertical: 12,
   },
   router: {
-    backgroundColor: "#FF783F",
+    backgroundColor: "#FEF6AE",
     alignItems: "center",
     color: "#D12121",
+    
   },
 });
+*/
