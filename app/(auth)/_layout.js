fe363f147508c34d1d17fb3b49ab6014a23ffa87
Nhuengzii <32nhueng@gmail.com>
2023-04-21 20:26:17 +0700

    remove everything

--- app/(auth)/_layout.js
+++
@@ -1,9 +0,0 @@
-import { Stack } from "expo-router";
-
-export default function Layout() {
-  return <Stack screenOptions={{ headerShown: false }}>
-    <Stack.Screen name="sign-in" />
-    <Stack.Screen name="register" />
-    <Stack.Screen name="agreement" options={{ presentation: "modal" }} />
-  </Stack>
-}
