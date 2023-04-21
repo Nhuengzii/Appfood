49ebea9dc90676c4e5337533c650fa2d1cd7d6e7
Nhuengzii <32nhueng@gmail.com>
2023-04-21 15:25:51 +0700

    restructure layout and create template for register agreement.

---
+++ app/(auth)/agreement.js
@@ -0,0 +1,13 @@
+import { Text } from "react-native"
+
+export default function agreement() {
+  return (
+    <Text style={{ fontSize: 55 }}>
+      เอาเงิน เอาเงินเยอะๆ
+      เอาเงิน เอาเงินเยอะๆ
+      เอาเงิน เอาเงินเยอะๆ
+      เอาเงิน เอาเงินเยอะๆ
+      เอาเงิน เอาเงินเยอะๆ
+    </Text>
+  )
+}
