b405a8b76803c9c71951d6f0b5712b01118a261a
mahasachan <mahasachan.sa@ku.th>
2023-05-03 17:33:49 +0700

    add component SmileIcon

---
+++ components/SmileIcon.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Text, View, StyleSheet, Pressable, ScrollView, Image } from "react-native";
+import { FontAwesome5 } from '@expo/vector-icons'; 
+
+export default function SmileIcon({ color }) {
+    return (
+        <View style={styles.containIcon}>
+            <View style={{
+                backgroundColor: color,
+                width: 51,
+                height: 51,
+                borderRadius: 51,
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}>
+                <FontAwesome5 style={styles.icon} name="smile" size={50} color="black" />
+            </View>
+        </View>
+                
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#E9FFDF",
+    },
+    containIcon: {
+        backgroundColor: "#FFF",
+        width: 60,
+        height: 60,
+        borderRadius: 60,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    icon: {
+        margin: 0,
+        padding: 0,
+        alignSelf: 'center',
+    },
+});
